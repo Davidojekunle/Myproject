cbb31237dada91aa99b2bc5ad904dc271dff3bf1
Davidojekunle <davidoiekunle@gmail.com>
2024-02-26 10:18:13 +0100

    projects

---
+++ monday1.py
@@ -0,0 +1,19 @@
+'''functions 
+this is a block of code that is designed to perform a specific task
+how does it works 
+it works woth a '''
+def david(name):
+      
+    print(f"hello {name}")
+david("mike")
+
+#we can call two variables on one line
+
+def name(a =4,b=7):
+    print(a+b)
+name(2,9)
+
+#lambda
+x = lambda a : a + 10
+number = int(input(""))
+print(x(nmber))
